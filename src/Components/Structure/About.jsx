4b3e00f53fd2db80105d38b5ed8b193b8b9e5800
Vladyslav Makartet <vladyslav.makartet@gmail.com>
2023-09-21 21:51:18 -0400

    Update node and dependencies

--- src/Components/Structure/About.jsx
+++ src/Components/Structure/About.jsx
@@ -17,16 +17,16 @@ const About = () => {
                     <p>With this web app, you can create recipes and perform calculations of each ingredient you need to buy to cook a meal (available in the Shopping section). </p>
                     <p>The program allows to view and edit each recipe created as well as ingredients belonging to a certain recipe.</p>
                     <p>Additionally, it is possible to load and automatically save created recipes on the server.</p>
-                    <p>The app uses a source-available cross-platform document-oriented database program MongoDB, a cloud platform as a service Heroku and is hosted on Netlify.</p>
+                    <p>The app uses a source-available cross-platform document-oriented database program MongoDB, a cloud platform as a service <s>Heroku</s> Cyclic and is hosted on Netlify.</p>
                     <p>The web application is one of the assignments for the EGUI course at <LinkMaterial  href="https://www.pw.edu.pl/engpw">Warsaw University of Technology</LinkMaterial>.</p>
                     <h1 className="title text-left">Tools used</h1>
                     <ul className="list">
-                        <li>Microsoft Visual Studio Code Version 1.56.2</li>
-                        <li>Node.js Version 14.16.0</li>
-                        <li>React Version 17.0.2</li>
-                        <li>Material-UI Version 4.11.4</li>
-                        <li>MongoDB Version 4.4.5</li>
-                        <li>Heroku</li>
+                        <li>Microsoft Visual Studio Code Version 1.81.1</li>
+                        <li>Node.js Version 18.18.0</li>
+                        <li>React Version</li>
+                        <li>Material-UI Version</li>
+                        <li>MongoDB</li>
+                        <li><s>Heroku</s> Cyclic</li>
                         <li>Netlify </li>
                     </ul>
                     <Link to='/'> Go Back</Link>
